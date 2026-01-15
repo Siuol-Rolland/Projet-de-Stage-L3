ba38479b614bc0d52e70e10f68e563ec8321889e
Siuol-Rolland <rollandsiuol@gmail.com>
2026-01-15 14:56:44 +0300

    new version3

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
-// import type { WebpackError } from "webpack";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+
+  // ⛔ DÉSACTIVE TURBOPACK
+  turbopack: {},
 
   serverExternalPackages: [
     "@prisma/client",
@@ -29,36 +31,15 @@ const nextConfig: NextConfig = {
   },
 
   experimental: {
-    authInterrupts:true,
+    authInterrupts: true,
     serverActions: {
       bodySizeLimit: "30mb",
     },
   },
 
-  // ✅ TU L’AS DÉJÀ (OK)
   typescript: {
     ignoreBuildErrors: true,
   },
-
-  // webpack: (config, { isServer }) => {
-  //   if (!isServer) {
-  //     config.resolve.fallback = {
-  //       fs: false,
-  //       path: false,
-  //       os: false,
-  //     };
-  //   }
-
-  //   config.ignoreWarnings = [
-  //   //   (warning: WebpackError) =>
-  //   //     typeof warning.message === "string" &&
-  //   //     warning.message.includes(
-  //   //       "EPERM: operation not permitted, scandir 'C:\\Users\\WINDOWS 10\\Application Data'"
-  //   //     ),
-  //   // ];
-
-  //   return config;
-  // },
 };
 
 export default nextConfig;
